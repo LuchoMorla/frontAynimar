1d80f7cfebb0ad5808ce2e7e720597d7b840b3cb
morlucho <luis-fernando-10@outlook.es>
2022-05-09 15:57:30 -0500

    cambie ubicacion, rutas de alias y configuracion de tailwind.config.js

--- react-shop-admin/tailwind.config.js
+++ react-shop-admin/tailwind.config.js
@@ -3,7 +3,10 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     './src/pages/**/*.{html,js,jsx}',
-    './src/pages/components/**/*.{html,js,jsx}',
+    './src/components/**/*.{html,js,jsx}',
+    './src/layout/**/*.{html,js,jsx}',
+    './src/public/*.{ico,svg}',
+    './src/common/*.{js,jsx}',
     './src/styles/*.{css,scss}',
   ],
   theme: {

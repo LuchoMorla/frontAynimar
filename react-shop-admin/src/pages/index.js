f3c866a1bf25fe9d436a92b81743e2cb192e7e3f
morlucho <luis-fernando-10@outlook.es>
2022-05-09 16:52:40 -0500

    cree la carpeta dashboard/index.js agregue la estructura y la logica del dashboard, así como le agregue la logica al Login, finalmente agregue en sus respectivos links sus rutas

--- react-shop-admin/src/pages/index.js
+++ react-shop-admin/src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
               <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Company</a>
 
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
+              <a href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
             </div>
           </nav>
         </div>
@@ -55,7 +55,7 @@ export default function Home() {
 
               <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
             </div>
-            <a href="#" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a>
+            <a href="/login" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a>
           </div>
         </div>
       </div>

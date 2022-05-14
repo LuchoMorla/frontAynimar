62b1c62dcf43504010ffca0fa5926302a896f919
morlucho <luis-fernando-10@outlook.es>
2022-05-14 16:19:07 -0500

    Add Dinamic edit button 4 products

--- react-shop-admin/src/pages/dashboard/products.js
+++ react-shop-admin/src/pages/dashboard/products.js
@@ -114,7 +114,7 @@ export default function Products() {
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="/edit" className="text-indigo-600 hover:text-indigo-900">
+                        <a href={"edit/" + product.id} className="text-indigo-600 hover:text-indigo-900">
                           Edit
                         </a>
                       </td>

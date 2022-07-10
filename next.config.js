fafd40bdfc57033d0b5fea0afdba42979813f948
morlucho <luis-fernando-10@outlook.es>
2022-07-10 16:08:43 -0500

    course realize

--- next.config.js
+++ next.config.js
@@ -1,6 +1,43 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+
+const nextConfig = withPWA({
+  pwa: {
+    //indicamos el destino donde se van a encontrar nuestros archivos publicos
+    dest: 'public',
+    //indicamos si vamos a registrar o no un worker
+    register: true,
+    //modo que vamos a trabajar
+    mode: 'production',
+    //para habilitarlo segun sea el caso.
+    disable: false,
+  },
   reactStrictMode: true,
-}
+  images: {
+    domains: ['placeimg.com', 'api.lorem.space'],
+  },
+});
+
+/* const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['placeimg.com', 'api.lorem.space'],
+  },
+} */
 
 module.exports = nextConfig
+
+ /* env: {
+  customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+    async redirects() {
+      return [
+        {
+        source: '/hola',
+        destination: 'https://gndx.dev',
+        permanent: true,
+      }
+    ]
+  } */

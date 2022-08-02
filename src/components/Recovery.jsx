19da13d3e8db8cd01350a3c74f09506449d0bb9f
morlucho <luis-fernando-10@outlook.es>
2022-08-02 10:42:09 -0500

    the password confirmation is ready

--- src/components/Recovery.jsx
+++ src/components/Recovery.jsx
@@ -6,6 +6,27 @@ import logo from '@logos/logo-Aynimar.svg'
 import styles from '@styles/PasswordRecovery.module.scss';
 
 export default function Recovery() {
+    const formRef = useRef(null);
+
+    const auth = useAuth();
+    const router = useRouter(); 
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const formData = new FormData(formRef.current);
+        const data = {
+            password: formData.get('password'),
+            confirmPassword: formData.get('new-password')
+        }
+        let thePassword = data.password;
+        let theConfirmPassword = data.confirmPassword;
+        if(thePassword !== theConfirmPassword) {
+            alert('Las contraseñas no son iguales');
+        }
+        if(thePassword === theConfirmPassword) {
+            console.log('ok the password is: ' + data.password);
+        }
+      };
 
   return (
     <div className={styles.login}>
@@ -15,20 +36,27 @@ export default function Recovery() {
         <h1 className={styles.title}>Crea tu nueva contraseña</h1>
         <p className={styles.subtitle}>Ingresa una nueva contraseña para tu cuenta</p>
 
-        <form action="/" className={styles.form} >
+        <form action="/" ref={formRef} className={styles.form}  onSubmit={submitHandler}  >
             <label htmlFor="password" className={styles.label}>Contraseña</label>
             <input
             type="password" 
             id="password" 
-            placeholder="*********" 
-            className={(styles.input, styles['input-password'])} 
+            name="password" 
+            autoComplete="current-password" 
+            required
+            className={styles.input} 
+            placeholder="**********"
             />
 
-            <label htmlFor="new-password" className={styles.label}>Contraseña</label>
-            <input type="password" 
+            <label htmlFor="new-password" className={styles.label}>Confirma tu Contraseña</label>
+            <input 
+            type="password" 
             id="new-password" 
-            placeholder="*********" 
-            className={(styles.input, styles['input-password'])} 
+            name="new-password" 
+            autoComplete="current-password" 
+            required
+            className={styles.input} 
+            placeholder="**********"
             />
 
             <input type="submit" value="Confirm" className={(styles['primary-button'], styles['login-button'])} />

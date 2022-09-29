dcb41608e97723d3ccd4b2e3c7c112bec731d369
morlucho <luis-fernando-10@outlook.es>
2022-09-29 07:34:30 -0500

    add image url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['api.lorem.space', 'placeimg.com'],
+    domains: ['api.lorem.space', 'placeimg.com', 'i.pinimg.com'],
   }
 };
 

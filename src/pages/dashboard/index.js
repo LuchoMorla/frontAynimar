f06e67bbc6a20f0bf1581c0ab3f3c399fabaad56
morlucho <luis-fernando-10@outlook.es>
2022-09-29 11:38:59 -0500

    change name componente Pagination

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import endPoints from '@services/api'
 import useFetch from '@hooks/useFetch';
-import Pagination from '@components/Pagination';
+import Pagination from '@components/Pagination.jsx';
 import { Chart } from '@common/Chart';
 
   export default function Dashboard() {

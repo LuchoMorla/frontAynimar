1f0c9f64e4b457f74000f793a019d43b4d4285d9
diamond9159 <johnford9159@gmail.com>
2023-05-11 18:19:53 -0700

    minor fix

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, createContext, useEffect } from 'react';
+import React, { useState, useContext, createContext } from 'react';
 import Cookie from 'js-cookie';
 import axios from 'axios';
 import endPoints from '@services/api/';

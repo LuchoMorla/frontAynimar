2acb9463508f956084197dc85929e1670183138d
morlucho <luis-fernando-10@outlook.es>
2023-05-21 16:32:01 -0500

    corregi dos semmicolom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,11 +24,11 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url);
-    }
+    };
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
-    }
+    };
   }, [router.events]);
 
   return (

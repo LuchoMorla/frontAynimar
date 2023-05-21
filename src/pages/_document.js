6d452484154de90db17443b99c91d97f20929e77
morlucho <luis-fernando-10@outlook.es>
2023-05-21 14:39:01 -0500

    segundo intento de funcionar analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -9,6 +10,13 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/static/favicon.ico" />
         </Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6S5D2T7858" />
+        <Script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-6S5D2T7858');
+        </Script>
         <body>
           <Main />
           <NextScript />

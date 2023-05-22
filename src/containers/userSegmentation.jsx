9030d55750eb08f3527b8826c3c8872fd87c8776
morlucho <luis-fernando-10@outlook.es>
2023-05-22 05:09:04 -0500

    añadimos cambios en la vista del celu

--- src/containers/userSegmentation.jsx
+++ src/containers/userSegmentation.jsx
@@ -16,8 +16,8 @@ const userSegmentation = () => {
     </section> */}
     <section className={styles['Customer_segment-container']}>
         <div className={styles.ContainerCallToAction}>
-            <h1 className={styles.hiTitle}>Compra de forma segura Online</h1>
-            <p>¡Bienvenido(a) a Aynimar! Explora nuestra amplia selección de productos. {'\n'} ¡Regístrate ahora y comienza a {/* disfrutar de la mejor experiencia de */} comprar en línea!</p>
+            <h1 className={styles.hiTitle}>{/* Compra de forma segura Online */}Pide ya!!</h1>
+            <p>¡Bienvenido(a) a Aynimar! Explora nuestra amplia selección de productos. {'\n'} ¡Regístrate ahora y comienza a {/* disfrutar de la mejor experiencia de */} comprar en línea de forma segura!</p>
             {/* <p>Compra desde tu computadora o telefono productos que son responsables con el medio ambiente y contribuyen a la creación de una economia circular y verde.</p> */}
         </div>
         <div className={styles.buttonr}>

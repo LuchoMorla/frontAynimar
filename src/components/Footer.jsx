3f96c8b34465652abd04ff2c9139f1e5bad660ec
morlucho <luis-fernando-10@outlook.es>
2023-05-22 03:08:33 -0500

    creamos nuevo footer, con un mensaje sobre entregas y botones de registrate y desplazamiento atraso y adelante

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 import Script from 'next/script';
-import styles from '@styles/Header.module.scss';
+import { useRouter } from 'next/router';
+import styles from '@styles/Footer.module.scss';
 
 const Footer = () => {
+	const router = useRouter();	
+	const currentPath = router.pathname;
 
 	return (
 		<>
@@ -11,11 +14,20 @@ const Footer = () => {
 			<div className={styles['navbar-left']}>
 				<ul>
 					<li>
-                        <div id="widgets"></div>
-                        <Script src='https://www.hazvaca.com/project/widgets_code/s/red/3408'
-                                type='text/javascript'></Script>
+						<button /* href="/recycling" */ onClick={() => { router.back(); }}  className={styles.footerLink}>
+								Atras
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" width={50} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  					<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+              					</svg>
+							</button>		
 					</li>
 					<li>
+                        {/* <div id="widgets" className={styles.widgets}></div>
+                        <Script src='https://www.hazvaca.com/project/widgets_code/s/red/3408'
+                                type='text/javascript'></Script> */}
+						<p className={styles.textoFooter}>Estamos haciendo entregas inmediatas actualmente en la ciudad de Quito, nos comunicaremos en caso de poder o no poder realizar la entrega en su ciudad o País.</p>
+					</li>
+{/* 					<li>
 						<Link href="/recycling">Reciclar</Link>
 					</li>
 					<li>
@@ -26,16 +38,26 @@ const Footer = () => {
 					</li>
 					<li>
 						<Link href="/">Blog</Link>
-					</li>
+					</li> */}
 				</ul>
 			</div>
 			<div className={styles['navbar-right']}>
-				<ul>
-					<li>			
+				<ul className={styles.footerRigthList}>
+					<li>
+						<Link href="/signInCustomer" className={styles.footerLink}>Registrarse</Link>			
 					</li>
 					<li>
-					</li>
-					<li>
+						<button /* href="/recycling" */ 
+						onClick={() => {/* 
+								console.log('i can see your current path is this one: ', currentPath);  */
+								currentPath == '/home' ? router.push('/store') : currentPath == '/store' ? router.push('/checkout') : currentPath == '/checkout' ? router.push('/mi_cuenta/cliente') : router.push('/mi_cuenta/orders');
+							}}
+						className={styles.footerLink}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none"  width={50} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  					<path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              					</svg>
+								Adelante
+							</button>		
 					</li>
 				</ul>
 			</div>
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '@components/Header';
+import Footer from '@components/Footer';
+import styles from '@styles/Layout.module.scss';
+
 const Layout = ({ children }) => {
 	return (
 		<div className="Layout">
 			<Header />
-			{children}
+			<div className={styles.content}>
+				{children}
+			</div>		
+			<Footer />
 		</div>
 	);
 }

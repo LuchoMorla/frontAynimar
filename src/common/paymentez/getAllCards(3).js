3613208ac7c623ec02c395fcdb246d8c7d5aba1f
morlucho <luis-fernando-10@outlook.es>
2023-05-28 05:25:09 -0500

    ultimo build

--- src/common/paymentez/getAllCards(3).js
+++ src/common/paymentez/getAllCards(3).js
@@ -1,5 +1,6 @@
-const axios = require('axios');
+/* const axios = require('axios');
 const codePaymentezNuvei = process.env.NEXT_PUBLIC_API_PAYMENTEZ_API_CODE_S;
 const keyPaymentezNuvei = process.env.NEXT_PUBLIC_API_PAYMENTEZ_API_KEY_S;
 
  var urlrefund = await `https://ccapi.paymentez.com/v2/card/list?uid=${id}`;
+ */

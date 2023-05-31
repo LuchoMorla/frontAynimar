4391f6324d96db9d36ca97f361620c15fa0a6141
morlucho <luis-fernando-10@outlook.es>
2023-05-30 19:22:54 -0500

    nuevo delete button en myOrder

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,30 +1,51 @@
-import React/* , { useContext } */ from 'react';
+import React, { useContext } from 'react';
 import Image from 'next/image';
-/* import AppContext from '@context/AppContext';
-import close from '@icons/icon_close.png'; */
+import AppContext from '@context/AppContext';
+import close from '@icons/icon_close.png';
+import endPoints from '@services/api/index';
+import axios from 'axios';
 import styles from '@styles/OrderItem.module.scss';
-/* import endPoints from '@services/api/index';
-import axios from 'axios'; */
 
 const OrderItem = ({ product }) => {
+	const { removeFromCart } = useContext(AppContext);
+
 	const orderProduct = product.OrderProduct;
 	const amount = orderProduct.amount;
-/* 	const handleEdit = someID => {
 
-	}; */
+	const removeItemProduct = async (item) => {
+		const config = {
+		  headers: {
+			accept: '*/*',
+			'Content-Type': 'application/json',
+		  },
+		};
+		const deleteItem = await axios.delete(endPoints.orders.deleteItem(item), config);
+		return deleteItem.data;
+	  };
+	
+	const handleRemove = (product) => {
+		removeFromCart(product);
+		const orderProductId = product.OrderProduct.id;
+		if (!orderProductId) {
+		  console.log('se cago, no accedi');
+		}
+		removeItemProduct(orderProductId);
+	  };
+
 	return (
 		<div className={styles.OrderItem}>
 			<figure>
 				<Image src={product?.image} width={40} height={40} alt={product?.name} />
 			</figure>
-			{/* <p onClick={() => {}}>Edit</p> */}
 			<p>{product?.name}</p>
-			<p>${product?.price}</p>	
-			{amount != null ? <p>x {amount}</p> : null}
-{/* 			<Image className={(styles.pointer, styles['more-clickable-area'])}
-			src={close}
-			alt="close"
-			onClick={() => handleRemove(product)} /> */}
+			{amount != null ? <p>${product?.price} x {amount}</p> : <p>${product?.price}</p>}
+			<Image className={styles['more-clickable-area']}
+				src={close} 
+				alt="close" 
+				width={18} 
+				height={18} 
+				onClick={() => handleRemove(product)} 
+			/>
 		</div>
 	);
 };

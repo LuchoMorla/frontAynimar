197300bfff10554323e1cbf79afe36fe1eccff6a
morlucho <luis-fernando-10@outlook.es>
2023-05-30 21:35:37 -0500

    nuevos agregados y cambios en my order incluyendo calculo de total y myOrders

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import OrderItem from '@components/OrderItem';
 import AppContext from '@context/AppContext';
 import arrow from '@icons/flechita.svg';
+import actualizarImg from '@icons/button_refresh_15001.png';
 import styles from '@styles/MyOrder.module.scss';
 
 const MyOrder = () => {
@@ -14,7 +15,22 @@ const MyOrder = () => {
 		const sum = state.cart.reduce(reducer, 0);
 		return sum.toFixed(2);
 	} */
-	
+	const sumTotal = () => {
+		//const reducer = (accumalator, currentValue) => accumalator + currentValue.price * currentValue.OrderProduct['amount'];
+		const reducer = (accumalator, currentValue) => {
+		  if (currentValue.price && currentValue.OrderProduct && currentValue.OrderProduct.amount) {
+			return accumalator + currentValue.price * currentValue.OrderProduct.amount;
+		  } else {
+			return accumalator;
+		  }
+		};
+		const sum = state.cart.reduce(reducer, 0);
+		return sum.toFixed(2);
+	  };
+
+	let valorTotalSinIva = sumTotal();
+/* 
+	let valorTotalConIva = valorTotalSinIva * 1.12; */
 	return (
 		<aside className={styles.MyOrder}>
 			<div className={styles['MyOrder-container']}>
@@ -36,6 +52,17 @@ const MyOrder = () => {
 						</p>
 						<p>${sumTotal()}</p>
 					</div> */}
+				<div className={styles.order}>
+                  <p>
+                    <span>Total:</span>
+                  </p>
+                  <p>
+                    ${valorTotalSinIva}{' '}
+                    <button onClick={() => router.reload()} className={styles.reloadButton}>
+                      <Image src={actualizarImg} alt='Actualizar | Update' width={20} height={20} />
+                    </button>
+                  </p>
+                </div>
 					<Link href="/checkout">
 						<button className={styles['primary-button']} >Checkout</button>
 					</Link>

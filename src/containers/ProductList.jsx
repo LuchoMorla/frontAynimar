9cd039fd7bd528b5df38f6943aafe0ecbc02d0c0
morlucho <luis-fernando-10@outlook.es>
2023-06-07 19:42:08 -0500

    fixing all bugs

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import endPoints from '@services/api/index';
 import useFetchProducts from '@hooks/useGetProducts';
 import ProductItem from '@components/ProductItem';
@@ -6,40 +6,31 @@ import Paginacion from '@common/Paginacion';
 import styles from '@styles/ProductList.module.scss';
 
 const ProductList = () => {
-  const PRODUCT_LIMIT = 16;
-  const [products, setProducts] = useState([]);
-  const [PRODUCT_OFFSET, setOffsetProducts] = useState(0);
-  const [totalProducts, setTotalProducts] = useState(0);
+	const PRODUCT_LIMIT = 16;
+/* 	const PRODUCT_OFFSET = 0; */
+	const [PRODUCT_OFFSET, setOffsetProducts] = useState(0);
+	
+	const products = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET), PRODUCT_OFFSET);
+	const totalProducts = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)).length;
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await useFetchProducts(
-        endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)
-      );
-      setProducts(response.products);
-      setTotalProducts(response.total);
-    };
+	useEffect(() => {
+		try {
+		  fecthData();
+		} catch (error) {
+		  console.log(error);
+		}
+	  }, [endPoints]);
 
-    fetchProducts();
-  }, [PRODUCT_OFFSET]);
-
-  return (
-    <section className={styles['main-container']}>
-      <div className={styles.ProductList}>
-        {products.map((product) => (
-          <ProductItem product={product} key={product.id} />
-        ))}
-      </div>
-      {totalProducts > 0 && (
-        <Paginacion
-          totalItems={totalProducts}
-          itemsPerPage={PRODUCT_LIMIT}
-          setOffset={setOffsetProducts}
-          neighbours={3}
-        />
-      )}
-    </section>
-  );
-};
+	return (
+		<section className={styles["main-container"]}>
+			<div className={styles.ProductList}>
+				{products.map(product => (
+					<ProductItem product={product} key={product.id} />
+				))}
+			</div>
+			{totalProducts > 0 && <Paginacion totalItems={totalProducts} itemsPerPage={PRODUCT_LIMIT} setOffset={setOffsetProducts} neighbours={3}></Paginacion>}
+		</section>
+	);
+}
 
 export default ProductList;

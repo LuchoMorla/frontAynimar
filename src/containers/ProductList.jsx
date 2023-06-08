14fa0223fb55639a3263ecd4b02b70f98f492413
morlucho <luis-fernando-10@outlook.es>
2023-06-07 19:17:45 -0500

    fixbugs and add useEffect to listen changes

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import endPoints from '@services/api/index';
 import useFetchProducts from '@hooks/useGetProducts';
 import ProductItem from '@components/ProductItem';
@@ -9,9 +9,12 @@ const ProductList = () => {
 	const PRODUCT_LIMIT = 16;
 /* 	const PRODUCT_OFFSET = 0; */
 	const [PRODUCT_OFFSET, setOffsetProducts] = useState(0);
+	const totalProducts = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)).length;
+	useEffect(() => {
+		const products = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET), PRODUCT_OFFSET);
+		return products
+	}, [PRODUCT_OFFSET]);
 
-	const products = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET), PRODUCT_OFFSET);
-	const totalProducts = useFetchProducts(endPoints.products.getProducts(10000000,0)).length;
 
 	return (
 		<section className={styles["main-container"]}>
@@ -20,7 +23,7 @@ const ProductList = () => {
 					<ProductItem product={product} key={product.id} />
 				))}
 			</div>
-			{totalProducts > 0 && <Paginacion totalItems={totalProducts} itemsPerPage={PRODUCT_LIMIT} setOffset={setOffsetProducts} neighbours={1}></Paginacion>}
+			{totalProducts > 0 && <Paginacion totalItems={totalProducts} itemsPerPage={PRODUCT_LIMIT} setOffset={setOffsetProducts} neighbours={3}></Paginacion>}
 		</section>
 	);
 }

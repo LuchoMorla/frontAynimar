b9011cb071eb5e830fe6d8e2b5087dc903409a47
morlucho <luis-fernando-10@outlook.es>
2023-06-07 19:34:02 -0500

    fix?

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -6,29 +6,40 @@ import Paginacion from '@common/Paginacion';
 import styles from '@styles/ProductList.module.scss';
 
 const ProductList = () => {
-	const PRODUCT_LIMIT = 16;
-/* 	const PRODUCT_OFFSET = 0; */
-	const [products, setProducts] = useState([]);
-	
-	const [PRODUCT_OFFSET, setOffsetProducts] = useState(0);
+  const PRODUCT_LIMIT = 16;
+  const [products, setProducts] = useState([]);
+  const [PRODUCT_OFFSET, setOffsetProducts] = useState(0);
+  const [totalProducts, setTotalProducts] = useState(0);
 
-	const totalProducts = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)).length;
-	useEffect(() => {
-		const products = useFetchProducts(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET), PRODUCT_OFFSET);
-		setProducts(products);
-	}, [PRODUCT_OFFSET]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await useFetchProducts(
+        endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)
+      );
+      setProducts(response.products);
+      setTotalProducts(response.total);
+    };
 
+    fetchProducts();
+  }, [PRODUCT_OFFSET]);
 
-	return (
-		<section className={styles["main-container"]}>
-			<div className={styles.ProductList}>
-				{products.map(product => (
-					<ProductItem product={product} key={product.id} />
-				))}
-			</div>
-			{totalProducts > 0 && <Paginacion totalItems={totalProducts} itemsPerPage={PRODUCT_LIMIT} setOffset={setOffsetProducts} neighbours={3}></Paginacion>}
-		</section>
-	);
-}
+  return (
+    <section className={styles['main-container']}>
+      <div className={styles.ProductList}>
+        {products.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
+      </div>
+      {totalProducts > 0 && (
+        <Paginacion
+          totalItems={totalProducts}
+          itemsPerPage={PRODUCT_LIMIT}
+          setOffset={setOffsetProducts}
+          neighbours={3}
+        />
+      )}
+    </section>
+  );
+};
 
 export default ProductList;

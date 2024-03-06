4e6dedead5c3088d1a98363395d6b8ba4ff7ec13
morlucho <luis-fernando-10@outlook.es>
2024-03-06 16:58:58 -0500

    optimizacion para SEO

--- src/pages/maquinasExpendedoras.js
+++ src/pages/maquinasExpendedoras.js
@@ -9,18 +9,18 @@ const VendingProspect = () => {
         <>
         <Head>
             <title>Aynimar | Maquinas Expendedoras</title>
-            <meta name="description" content="Rentabiliza tu espacio con las máquinas expendedoras de Aynimar. Llena el formulario para ponernos en contacto contigo." />
+            <meta name="description" content="Rentabiliza tu espacio con las máquinas expendedoras de Aynimar y obten ingresos pasivos. Buscamos un sitio para poner nuestras maquinas expendedoras a vender snacks y bebidas. Llena el formulario para ponernos en contacto contigo." />
         </Head>
-        <div className={styles.contact}>
+        <main className={styles.contact}>
         <Image src={logo} width={90} height={90} alt='Logo de Aynimar Maquinas Expendedoras' />
             <h1 className={styles.title}>Maquinas Expendedoras Aynimar</h1>
-            <p className={styles.text}>Cuentas con un lugar con transito de personas y te gustaría rentabilizar el espacio, sin tener que hacer ninguna inversion, mejorar el aspecto del espacio y además tener al alcance las 24/7 horas de snacks y bebidas?</p>
+            <p className={styles.text}>Si cuentas con un lugar con transito de personas, podrías rentabilizar el espacio, sin tener que hacer ninguna inversion, mejorar el aspecto del espacio, ganar dinero de forma pasiva y además tener al alcance las 24/7 horas de snacks y bebidas en el lugar de servicio</p>
             <div className={styles.imageContainer}>
             <Image className={styles.imageVending} src='https://vendingpassec.com/wp-content/uploads/2020/04/conocer-clientes-vending-1.jpg'  width={700} height={300} alt='Maquinas Expendedoras para aumentar la productividad en oficinas, hospitales, gymnasios, centros comerciales, etc.'  />
             </div>
-            <p className={styles.text}>Si tu respuesta es si, Continua llenando el siguiente formulario para que podamos ponernos en contacto con tigo y posicionar una de nuestras maquinas expendedoras en tú locacion.</p>
+            <p className={styles.text}>Si cuentas con un espacio, continua llenando el siguiente formulario para que podamos ponernos en contacto con tigo y posicionar una de nuestras maquinas expendedoras en tú locacion.</p>
             <FormContact />
-        </div>
+        </main>
         </>
     );
 };
--- src/pages/vendingForm.js
+++ src/pages/vendingForm.js
@@ -10,12 +10,12 @@ const VendingContact = () => {
         <Head>
             <title>Aynimar | Contacto Cliente Vending</title>
         </Head>
-        <div className={styles.contact}>
+        <main className={styles.contact}>
         <Image src={logo} width={90} height={90} alt='logo-Aynimar' />
             <h1 className={styles.title}>Maquinas Expendedoras Aynimar</h1>
             <p  className={styles.text}>Este es un servicio de gestion de experiencia de usuario, en la que aynimar se presta a escuchar sugerencias como productos nuevos, inconvenientes, soluciones y nuevos servicios con el fin de garantizarle siempre una mejor experiencia con nuestras maquinas expendedoras.</p>
             <FormContact />
-        </div>
+        </main>
         </>
     );
 };
